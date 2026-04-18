3a963cff0e6845d719694099cdde924e237a981e
Henry Savary-Jackson <96239862+Henry-Savary-Jackson@users.noreply.github.com>
2026-04-18 14:59:05 +0200

    final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import math
+from copy import deepcopy
 
 k_straight = 0.0000166
 k_braking = 0.0398 
@@ -18,12 +19,59 @@ current_weather = dict()
 
 weathers = list()
 
-craw_constant = 0
+crawl_constant = 0
 
 def read_race_file(file_path):
     global car , race, tyres,segments, weathers, current_weather
     with open(file_path, "r") as file:
         full_data = json.load(file)
+        race = full_data["race"]
+        car  = full_data["car"]
+        
+        craw_constant = car["crawl_constant_m/s"]
+        for seg in full_data["track"]["segments"]:
+            segment = {
+                "id" : seg["id"],"type" : seg["type"],"length" : seg["length_m"],}
+            if seg["type"] == "corner":
+                segment["radius"] = seg ["radius_m"]
+            segments.append(segment) # cleanned up segments appeded
+        
+        tyre_properties = full_data["tyres"]["properties"]
+
+        for available_set in full_data["available_sets"]:
+            compound = available_set["compound"]
+            props = tyre_properties[compound]
+
+            for tyre_id in available_set["ids"]:
+                tyres[tyre_id] = {
+                    "id":       tyre_id,
+                    "compound": compound,
+
+                    "life_span":    props["life_span"],
+                    "degradation":  0.0,
+
+                    "dry_friction_multiplier":        props["dry_friction_multiplier"],
+                    "cold_friction_multiplier":       props["cold_friction_multiplier"],
+                    "light_rain_friction_multiplier": props["light_rain_friction_multiplier"],
+                    "heavy_rain_friction_multiplier": props["heavy_rain_friction_multiplier"],
+
+                    "dry_degradation":        props["dry_degradation"],
+                    "cold_degradation":       props["cold_degradation"],
+                    "light_rain_degradation": props["light_rain_degradation"],
+                    "heavy_rain_degradation": props["heavy_rain_degradation"],
+                }
+        for condition in full_data["weather"]["conditions"]:
+            weathers.append({
+                "id":                    condition["id"],
+                "condition":             condition["condition"],
+                "duration_s":            condition["duration_s"],
+                "acceleration_multiplier": condition["acceleration_multiplier"],
+                "deceleration_multiplier": condition["deceleration_multiplier"],
+            })
+        starting_id = full_data["race"]["starting_weather_condition_id"]
+        current_weather.update(next(w for w in weathers if w["id"] == starting_id))   
+
+
 
 # tire friction calculation
 def straight_degradation(tyre_degradation, straight_distance):
@@ -52,8 +100,8 @@ def refueling_time(refuel_amount, refuel_rate):
     return time
 
 
-def max_corner_speed(tyre_degradation, straight_distance, initial_speed, final_speed, radius, bese_friction_coefficient, weather_multiplier, crawl_constant):
-    tyre_friction = total_tyre_friction(tyre_degradation, straight_distance, initial_speed, final_speed, radius, bese_friction_coefficient, weather_multiplier)
+def max_corner_speed(tyre_degradation,  radius, base_friction_coefficient, weather_multiplier, crawl_constant):
+    tyre_friction = total_tyre_friction(tyre_degradation,  base_friction_coefficient, weather_multiplier)
     max_speed = math.sqrt(tyre_friction * radius * gravity) + crawl_constant
     return max_speed
 
@@ -69,88 +117,44 @@ def score_level_2_and_3(time_reference, time, fuel_used,fuel_soft_cap_limit_l):
     fuel_bonus = -500000 * (1 - fuel_used/fuel_soft_cap_limit_l)**2 + 500000
     final_score = base_score_level_1(time_reference, time) + fuel_bonus
     return final_score
-    race = full_data["race"]
-    car  = full_data["car"]
     
-    craw_constant = car["crawl_constant"]
-    for seg in full_data["track"]["segments"]:
-        segment = {
-            "id" : seg["id"],"type" : seg["type"],"length" : seg["length_m"],}
-        if seg["type"] == "corner":
-            segment["radius"] = seg ["radius_m"]
-        segments.append(segment) # cleanned up segments appeded
-    
-    tyre_properties = full_data["tyres"]["properties"]
-
-    for available_set in full_data["available_sets"]:
-        compound = available_set["compound"]
-        props = tyre_properties[compound]
-
-        for tyre_id in available_set["ids"]:
-            tyres[tyre_id] = {
-                "id":       tyre_id,
-                "compound": compound,
-
-                "life_span":    props["life_span"],
-                "degradation":  0.0,
-
-                "dry_friction_multiplier":        props["dry_friction_multiplier"],
-                "cold_friction_multiplier":       props["cold_friction_multiplier"],
-                "light_rain_friction_multiplier": props["light_rain_friction_multiplier"],
-                "heavy_rain_friction_multiplier": props["heavy_rain_friction_multiplier"],
-
-                "dry_degradation":        props["dry_degradation"],
-                "cold_degradation":       props["cold_degradation"],
-                "light_rain_degradation": props["light_rain_degradation"],
-                "heavy_rain_degradation": props["heavy_rain_degradation"],
-            }
-    for condition in full_data["weather"]["conditions"]:
-        weathers.append({
-            "id":                    condition["id"],
-            "condition":             condition["condition"],
-            "duration_s":            condition["duration_s"],
-            "acceleration_multiplier": condition["acceleration_multiplier"],
-            "deceleration_multiplier": condition["deceleration_multiplier"],
-        })
-    starting_id = full_data["race"]["starting_weather_condition_id"]
-    current_weather.update(next(w for w in weathers if w["id"] == starting_id))   
-
-
 def score_level_4(base_friction_coefficient, weather_multiplier, accumulated_degradation, number_of_blowouts):
     tyre_bonus = 100000 * total_tyre_friction(base_friction_coefficient, accumulated_degradation, weather_multiplier) - 50000 * number_of_blowouts
     return tyre_bonus
 
 def main():
     read_race_file("1.txt")
+    current_lap = 0
+    output_dict = dict()
+    output_dict = decision_at_lap(race["laps"], output_dict, tyres, 0, 0 )
+    with open("output.txt","w") as file:
+        json.dump(output_dict, file)
 
-    print(car)
-    print(f"Race lap{race['laps']}")
-    for i in segments:
-        print(i)
 
-def decision_at_lap(lap, output_dict, tyre, tyres, time, fuel_used, wear, weather, speed):
+def decision_at_lap(laps, output_dict,  tyres, time,  speed):
     outputs = []
     # consider refuel
     # combinations of refuel no refuel , and change in tyre
     # enter pit or no
-    tyre = deepcopy(tyre)
-    tyres = deepcopy(tyres)
-    output_dict = deepcopy(output_dict)
 
+    for new_tyre  in tyres.values():
+        output_dict = deepcopy(output_dict)
+        tyre_output , tyre_time, tyre_speed = dict(),0, 0
+        output_dict["laps"] = []
 
-    for new_tyre, change in tyres:
-
-        # ignore if not changing but tire worn out
-        if not change and new_tyre["degradation"] <= 0:
-            continue
-
+        for lap in range(laps):
+            output_dict["initial_tyre_id"] = new_tyre["id"]
+            laps_dict = dict()
+            laps_dict ["lap"] = lap
+            output_dict["laps"].append(laps_dict)
+            
         # reduce number of tyres
         # calc lap 
-        lap_decision, lap_time, lap_fuel, lap_wear, lap_weather,lap_speed = calc_lap(lap, output_dict, tyre, time, fuel_used, wear, weather, speed)
+            tyre_output, tyre_time, tyre_speed = calc_lap(lap, tyre_output, new_tyre, time,  tyre_speed)
         # the recursively continue con
-        race_decision, race_time, race_fuel, fuel_needed_at_lap , race_wear = decision_at_lap(lap+1, lap_decision, tyre, tyres, lap_time, lap_fuel, lap_wear,lap_weather,lap_speed  )
+        # race_decision, race_time, race_fuel, fuel_needed_at_lap , race_wear = decision_at_lap(lap+1, lap_decision, tyre, tyres, lap_time, lap_fuel, lap_wear,lap_weather,lap_speed  )
         # calculate
-        outputs.append( (race_decision, race_time, race_fuel,fuel_needed_at_lap, race_wear ))
+        outputs.append( (tyre_output, tyre_time  ))
         # how much needed at last lap
         
    # get if not  
@@ -158,34 +162,57 @@ def decision_at_lap(lap, output_dict, tyre, tyres, time, fuel_used, wear, weathe
     max_score = 0
     for output in outputs:
         # get score, if greater than max, update max
-        score = base_score_level_1(0, output[1])
+        score = base_score_level_1(race["time_reference_s"], output[1])
         if score >= max_score:
-            max_output = output 
+            max_output = output[0] 
             max_score = score
 
     return max_output
 
+def get_time_straight(initial, final, accel):
+    return (final - initial) / accel
 
-
-def calc_lap(lap, output_dict,tyre ,time, fuel_used, wear,weather,speed):
+def calc_lap(lap, output_dict,tyre ,time, speed):
     # choose a tire if at all
     for i in range(len(segments)):
+        segment_dict = dict()
         segment = segments[i]
-        length = segment["length_m"]
+
+        segment_dict["id"] =segment["id"] 
+        segment_dict["type"] =segment["type"] 
+        length = segment["length"]
         seg_type = segment["type"]
-        accel = car["accel_m/e2"]
-        break_acc = car["brake_m/e2"]
+        accel = car["accel_m/se2"]
+        break_acc = car["brake_m/se2"]
         if (segment["type"] == "straight"):
             # choose
-            degradation = straight_degradation(tyre["degradation"], length)
+
             # get the new corner's speed
             if i != len(segments)-1:
                 corner = segments[i+1]
 
-                max_corner = max_corner_speed(tyre["dry_degradation"], ,weather["acceleration_multiplier"], speed,,corner["radius"] , craw_constant)
+                weather = weathers[0]
+                max_corner = max_corner_speed(tyre["dry_degradation"],  corner["radius"], k_base ,weather["acceleration_multiplier"], crawl_constant)
+
+                diff_speed = max_corner -speed
+                break_dist = diff_speed/2 * (diff_speed/accel)
+                delta_t = (abs(diff_speed)/break_acc) 
+                if diff_speed < 0 :
+                    break_dist = abs(diff_speed)/2 *  delta_t 
+                
+                break_point = max(0, length-break_dist)
+
+                time += (break_point/speed if speed else  0 )+  delta_t
+                if speed ==0:
+                    time += break_point/max_corner
+                speed = max_corner 
+                segment_dict["target_m/s"] = max_corner
+                segment_dict["brake_start_m_before_next/s"] = break_point 
+
 
         else:
             degradation = corner_degradation(speed,segment["radius"], tyre["dry_degradation"] )
+            time += segment["length"] / speed
     
         # choose target speed
 
@@ -203,12 +230,7 @@ def decision_tree():
     total_wear = 0
     weather = None # initial
     output_dict = dict()
-    current_lap = 0
 
-    return output_dict
 
-    print(f"Current weather {current_weather}\n")
-    for s in weathers:
-        print(s)
 if __name__ == "__main__":
     main()
