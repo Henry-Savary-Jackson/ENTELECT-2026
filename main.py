4034c97f049e08685cf2aa5fad69732af8cbdb34
Henry Savary-Jackson <96239862+Henry-Savary-Jackson@users.noreply.github.com>
2026-04-18 10:59:51 +0200

    basics

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 import json
 
 
+car = dict()
+
+tyres = dict()
+
+segements = list()
+
+current_weather = dict()
+
+weathers = dict()
+
+def read_race_file(file_path):
+    with open(file_path, "r") as file:
+        full_data = json.load(file)
+
+    
+
+def main():
+    read_race_file("example4.json") 
+
+
+if __name__ == "__main__":
+    main()
